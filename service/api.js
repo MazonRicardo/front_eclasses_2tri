bfd758f823e3dc2a320b59d67d3825641fbba319
MazonRicardo <ricardo.mazon@escola.pr.gov.br>
2026-06-22 08:28:31 -0300

    Update api.js

--- service/api.js
+++ service/api.js
@@ -2,41 +2,37 @@
 // Quando a API estiver pronta, basta trocar para: 'http://localhost:3000/api'
 const BASE_URL = 'http://localhost:3000/api/';
 
-async function trataErros(response) {
+async function _get(endpoint) {
     try {
+        const response = await fetch(`${BASE_URL}${endpoint}`)
         if (!response.ok) {
-            throw new Erorr(`HTTP error! status: ${response.statusText}`);
+            throw new Erorr(`Erro ao buscar os ${endpoint} ${response.statusText}`);
         }
         const data = await response.json();
         return data;
 
     } catch (error) {
-        console.error('Erro ao buuscar jogos:', error);
-        alert('Erro ao buscar jogos. Por favor, tente novamente mais tarde');
+        console.error(`Erro ao buuscar os ${endpoint}`, error);
         return[];
     }
 }
 
 // Retorna todos os jogos
 async function getJogos() {
-    const response = await fetch(`${BASE_URL}jogos`);
-    return trataErros(response);
+    return _get('jogos');
 }
 
 // Retorna todos os times
 async function getTimes() {
-    const response = await fetch(`${BASE_URL}times`);
-    return trataErros(response);
+    return _get('times');
 }
 
 // Retorna todos os competidores
 async function getCompetidores() {
-    const response = await fetch(`${BASE_URL}competidores`);
-    return trataErros(response);
+    return _get('competidores');
 }
 
 // Retorna todos os confrontos
 async function getConfrontos() {
-    const response = await fetch(`${BASE_URL}confrontos`);
-    return trataErros(response);
+    return _get('confrontos');
 }
