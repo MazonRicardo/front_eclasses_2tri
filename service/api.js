b0f7c150f96260e4f58d32408eecdf37e3fbffb4
MazonRicardo <ricardo.mazon@escola.pr.gov.br>
2026-06-22 07:54:11 -0300

    Função para tratamento de erro

--- service/api.js
+++ service/api.js
@@ -2,30 +2,41 @@
 // Quando a API estiver pronta, basta trocar para: 'http://localhost:3000/api'
 const BASE_URL = 'http://localhost:3000/api/';
 
+async function trataErros(response) {
+    try {
+        if (!response.ok) {
+            throw new Erorr(`HTTP error! status: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('Erro ao buuscar jogos:', error);
+        alert('Erro ao buscar jogos. Por favor, tente novamente mais tarde');
+        return[];
+    }
+}
+
 // Retorna todos os jogos
 async function getJogos() {
     const response = await fetch(`${BASE_URL}jogos`);
-    const data = await response.json();
-    return data;
+    return trataErros(response);
 }
 
 // Retorna todos os times
 async function getTimes() {
     const response = await fetch(`${BASE_URL}times`);
-    const data = await response.json();
-    return data;
+    return trataErros(response);
 }
 
 // Retorna todos os competidores
 async function getCompetidores() {
     const response = await fetch(`${BASE_URL}competidores`);
-    const data = await response.json();
-    return data;
+    return trataErros(response);
 }
 
 // Retorna todos os confrontos
 async function getConfrontos() {
     const response = await fetch(`${BASE_URL}confrontos`);
-    const data = await response.json();
-    return data;
+    return trataErros(response);
 }
